5febe93627d3c4cb4e3d004f0adef66fe6761a6e
fabianagenila <fabianagenila@gmail.com>
2022-06-14 11:35:06 -0300

    Adding code to components

---
+++ src/components/ShoppingFilter/ShoppingFilter.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ShoppingFilter = () => {
+  return (
+    <div>ShoppingFilter</div>
+  )
+}
+
+export default ShoppingFilter
---
+++ src/components/ShoppingList/Shoppinglist.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Shoppinglist = () => {
+  return (
+    <div>Shoppinglist</div>
+  )
+}
+
+export default Shoppinglist
